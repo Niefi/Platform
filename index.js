a463307a831c2b1269a367b74d596902a03b7b49
elvi <rantanenelvi@gmail.com>
2023-10-03 09:41:45 +0300

    second

--- index.js
+++ index.js
@@ -5,5 +5,19 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+
+
+let y = 100
+function animated(){
+    window.requestAnimationFrame(animated)
+
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+
+    c.fillStyle = 'red'
+    c.fillRect(200, y, 100, 100)
+    y++
+}
+
+animated()
